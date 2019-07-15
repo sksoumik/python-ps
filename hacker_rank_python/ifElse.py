2c71e97a48bd9486d477b1e83373ac945b56e4a5
sksoumik <sadmanks@gmail.com>
2019-07-15 11:28:20 +0600

    Hacker Rank Python - ifElse

---
+++ hacker_rank_python/ifElse.py
@@ -0,0 +1,18 @@
+"""
+https://www.hackerrank.com/challenges/py-if-else/problem
+"""
+
+import sys
+
+
+N = int(input().strip())
+
+if N % 2 == 0:
+    if N >= 2 and N <= 5:
+        print("Not Weird")
+    elif N >= 6 and N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird")
