b7034d8c052b25a1c2f7a7e9e161e52465aaec5d
sksoumik <sadmanks@gmail.com>
2019-07-15 11:35:03 +0600

    Hacker Rank Python - division

---
+++ hacker_rank_python/Arithmetic.py
@@ -0,0 +1,12 @@
+"""
+problem 03
+https://www.hackerrank.com/challenges/python-arithmetic-operators
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ hacker_rank_python/PythonDivision.py
@@ -0,0 +1,11 @@
+"""
+problem 04:
+https://www.hackerrank.com/challenges/python-division
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a//b)
+    print(a/b)
