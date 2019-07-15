58b27922e9d38caa9a639ad4186e091671bd1ade
sksoumik <sadmanks@gmail.com>
2019-07-15 11:56:15 +0600

    Hacker Rank Python - Function

---
+++ hacker_rank_python/Loops.py
@@ -0,0 +1,9 @@
+"""
+https://www.hackerrank.com/challenges/python-loops/problem
+"""
+
+N = int(input())
+
+for i in range(N):
+    print(i*i)
+
---
+++ hacker_rank_python/WriteFunction.py
@@ -0,0 +1,24 @@
+"""
+https://www.hackerrank.com/challenges/write-a-function/problem
+
+Leap year checking
+"""
+
+
+def is_leap(year):
+    leap = False
+
+    if (year % 4 == 0):
+        leap = True
+        if (year % 100 == 0):
+            leap = False
+            if (year % 400 == 0):
+                leap = True
+    return leap
+
+N = int(input())
+
+print(is_leap(N))
+
+
+
