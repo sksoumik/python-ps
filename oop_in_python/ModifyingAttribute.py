abacd8f8abd7103fa8fd22ee2b74329c51368bf2
sksoumik <sadmanks@gmail.com>
2019-07-18 12:23:16 +0600

    OOP in python - modify object

---
+++ oop_in_python/ModifyingAttribute.py
@@ -0,0 +1,20 @@
+"""
+ we can change the value of attributes based on some behavior
+"""
+
+class Email:
+    def __init__(self):
+        self.is_sent = False
+
+
+    def send_mail(self):
+        self.is_sent = True
+
+
+
+my_email = Email()
+print(my_email.is_sent)
+
+my_email.send_mail()
+print(my_email.is_sent)
+
