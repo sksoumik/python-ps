c8d911ed3aed9b8b53b46089cae5c6a107e9fdd8
sksoumik <sadmanks@gmail.com>
2019-08-20 23:19:38 +0600

    stack implementation using array

---
+++ data_structures/stack_implementation_array.py
@@ -0,0 +1,45 @@
+"""
+Example: stack of plate
+LIFO - Last in First out
+Insertion and Deletion both happen in the same end
+Basic Operations: push(), pop(), peek(), isEmpty() all take O(1) time
+"""
+from sys import maxsize
+
+# Create a stack
+def create_stack():
+    stack = []
+    return stack
+
+def isEmpty(stack):
+    return len(stack) == 0
+
+def push(stack, item):
+    stack.append(item)
+    print(item, "pushed onto stack")
+
+def pop(stack):
+    if isEmpty(stack):
+        return str(-maxsize - 1)
+    return stack.pop()
+
+def peek(stack):
+    if isEmpty(stack):
+        return str(-maxsize - 1)
+    return stack[len(stack)-1]
+
+# Driver code
+stack = create_stack()
+push(stack, 10)
+push(stack, 20)
+push(stack, 30)
+pop(stack) # removed 30
+print(stack)
+print(peek(stack))  # 20
+
+
+"""
+ref: https://www.geeksforgeeks.org/stack-data-structure-introduction-program/
+"""
+
+
