c22225df89be10d38742b10ff8017d3b591a649b
sksoumik <sadmanks@gmail.com>
2019-08-21 01:23:08 +0600

    check palindrome

---
+++ basic_programs/palindrome.py
@@ -0,0 +1,35 @@
+"""
+Checking if a string is palindrome ot not.
+"""
+
+
+# Process 01
+def isPalindrome(string):
+    flag = 0
+
+    length = len(string)
+    for i in range(length):
+        if (string[i] != string[length - i - 1]):
+            flag = 1
+            break
+
+    if (flag == 0):
+        print("This is a Palindrome String")
+    else:
+        print("This is Not a Palindrome String")
+
+
+# driver code
+sample = input("Please enter your own String : ")
+isPalindrome(sample)
+
+"""
+Procedure 02
+
+string = input()
+if string != string[::-1]:
+    print("Not palindrome")
+else:
+    print("Palindrome")
+
+"""
