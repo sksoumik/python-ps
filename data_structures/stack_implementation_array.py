8cfb2c2e3128a092b073f43f0e9398a2f405a994
sksoumik <sadmanks@gmail.com>
2019-08-20 23:20:01 +0600

    stack implementation using array

--- data_structures/stack_implementation_array.py
+++ data_structures/stack_implementation_array.py
@@ -6,40 +6,43 @@ Basic Operations: push(), pop(), peek(), isEmpty() all take O(1) time
 """
 from sys import maxsize
 
+
 # Create a stack
 def create_stack():
     stack = []
     return stack
 
+
 def isEmpty(stack):
     return len(stack) == 0
+
 
 def push(stack, item):
     stack.append(item)
     print(item, "pushed onto stack")
+
 
 def pop(stack):
     if isEmpty(stack):
         return str(-maxsize - 1)
     return stack.pop()
 
+
 def peek(stack):
     if isEmpty(stack):
         return str(-maxsize - 1)
-    return stack[len(stack)-1]
+    return stack[len(stack) - 1]
+
 
 # Driver code
 stack = create_stack()
 push(stack, 10)
 push(stack, 20)
 push(stack, 30)
-pop(stack) # removed 30
+pop(stack)  # removed 30
 print(stack)
 print(peek(stack))  # 20
-
 
 """
 ref: https://www.geeksforgeeks.org/stack-data-structure-introduction-program/
 """
-
-
