947f3ea4d0515c15b16eeee5047d11aa0fc78d00
sksoumik <sadmanks@gmail.com>
2019-09-25 19:16:27 +0600

    nested lists

--- hacker_rank_python/nested_lists.py
+++ hacker_rank_python/nested_lists.py
@@ -2,3 +2,25 @@
 https://www.hackerrank.com/challenges/nested-list/problem?h_r=next-challenge&h_v=zen
 """
 
+name = []
+grade = []
+for i in range(int(input())):
+    name.append(input())
+    grade.append(float(input()))
+
+m = min(grade)
+re = []
+for i in range(len(grade)):
+    if grade[i] == m:
+        grade[i] = 1000000000
+
+m = min(grade)
+
+for i in range(len(grade)):
+    if grade[i] == m:
+        re.append(name[i])
+
+re.sort()
+
+for i in re:
+    print(i)
