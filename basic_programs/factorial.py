0ff190a94138994bdbef91942f354b5938c038f4
sksoumik <sadmanks@gmail.com>
2019-09-28 14:38:30 +0600

    update

--- basic_programs/factorial.py
+++ basic_programs/factorial.py
@@ -1,8 +1,8 @@
 def fact(n):
-    if (n == 1 or n == 0):
+    if n == 1 or n == 0:
         return 1
     else:
-        return n * fact(n-1)
+        return n * fact(n - 1)
 
 
 sample = int(input())
