48012930e7989e45f0e70481373f0b7a579df3ee
soumik <sadmanks@gmail.com>
2019-12-16 21:16:16 +0600

    leetcode:reverse integer

---
+++ leetCode/reverse_integer.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/reverse-integer/
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        str_x = str(x)
+        is_minus = False
+        if str_x[0] == '-':
+            str_x = str_x[1:]
+            is_minus = True
+        
+        rev = str_x[::-1]
+
+        if is_minus:
+            rev = '-'+rev
+        rev = int(rev)
+        if rev >  2**31 -1 or rev < -2**31:
+            return 0
+        return rev
+
+
+rev = Solution()
+sample = input()
+print(rev.reverse(sample))
+
+
+
+
+
+        
