dd0cfc94589206f896b5b671d445ded7780129cf
Sadman Kabir Soumik <sadmanks@gmail.com>
2020-01-29 11:25:05 +0600

    pythonic way to write getter and setter

--- oop_in_python/getter_setter.py
+++ oop_in_python/getter_setter.py
@@ -1,17 +1,29 @@
-class GetSet:
-    def __init__(self, x):
-        self.x = x
+"""
+A pythonic way to write getter, setter and deleter in OOP. 
+"""
 
-    def getter(self):
+
+class GetSet:
+    def __init__(self):
+        self.x = None
+
+    @property
+    def value(self):
         return self.x
 
-    def setter(self, value):
-        self.x = value
+    @value.setter
+    def value(self, v):
+        self.x = v
 
-    def deleter(self):
+    @value.deleter
+    def value(self):
         del self.x
 
 
 if __name__ == "__main__":
-    obj = GetSet(10)
-    print(obj.getter())
+    obj = GetSet()
+    obj.value = "Sadman Kabir Soumik"  # setter is called
+    get = obj.value  # getter is called
+    print(obj.value)
+    del obj.value  # deleter is called
+    print(obj.value)  # gives error
