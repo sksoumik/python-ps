9f2c2ef4d5859b1a20dd53b9e202c30e32642796
Soumik <sadmanks@gmail.com>
2020-02-08 12:45:57 +0600

    update

--- leetCode/Map.py
+++ leetCode/Map.py
@@ -1,8 +1,11 @@
 seq = [1, 2, 3, 4]
-# result contains odd numbers of the list 
+# result contains odd numbers of the list
 result = filter(lambda x: x + 1, seq)
-print(list(result)) # Does nothing on seq as the lambda function does not hold any condition
+print(
+    list(result)
+)  # Does nothing on seq as the lambda function does not hold any condition
 # Output: [1, 2, 3, 4]
 # filter only works on condition, so we need to give some conditional statement with lambda
-result = filter(lambda x: x % 2 == 0, seq)  # returns the elements which are only divisible by two
-print(list(result)) # output: [2, 4]
+result = filter(lambda x: x % 2 == 0,
+                seq)  # returns the elements which are only divisible by two
+print(list(result))  # output: [2, 4]
--- leetCode/reverse_integer.py
+++ leetCode/reverse_integer.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/reverse-integer/
 
+
 class Solution:
     def reverse(self, x: int) -> int:
         str_x = str(x)
@@ -7,13 +8,13 @@ class Solution:
         if str_x[0] == '-':
             str_x = str_x[1:]
             is_minus = True
-        
+
         rev = str_x[::-1]
 
         if is_minus:
-            rev = '-'+rev
+            rev = '-' + rev
         rev = int(rev)
-        if rev >  2**31 -1 or rev < -2**31:
+        if rev > 2**31 - 1 or rev < -2**31:
             return 0
         return rev
 
@@ -21,9 +22,3 @@ class Solution:
 rev = Solution()
 sample = input()
 print(rev.reverse(sample))
-
-
-
-
-
-        
