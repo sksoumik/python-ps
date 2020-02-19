6cd8f04f9f04b1d76e40b025818eb2547c0ca035
Sadman Kabir Soumik <sadmanks@gmail.com>
2020-02-19 09:59:17 +0600

    croping image based on roi

--- opencv/roi.py
+++ opencv/roi.py
@@ -1,37 +1,35 @@
-import cv2 
-import numpy as np 
-  
-# Reading the input image 
-image = cv2.imread('xray.jpg', 0) 
-  
-cv2.waitKey(0) 
-  
-# Grayscale 
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
-  
-# Find Canny edges 
-edged = cv2.Canny(image, 30, 200) 
-cv2.waitKey(0) 
-  
-# Finding Contours 
-# Use a copy of the image e.g. edged.copy() 
-# since findContours alters the image 
-contours, hierarchy = cv2.findContours(edged,  
-    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-  
-cv2.imshow('Canny Edges After Contouring', edged) 
-cv2.waitKey(0) 
-  
-print("Number of Contours found = " + str(len(contours))) 
-  
-# Draw all contours 
-# -1 signifies drawing all contours 
-cv2.drawContours(image, contours, -1, (0, 255, 0), 3) 
-  
-cv2.imshow('Contours', image) 
-cv2.waitKey(0) 
-cv2.destroyAllWindows() 
+import cv2
+import numpy as np
+
+# Reading the input image
+image = cv2.imread('xray.jpg', 0)
+
+cv2.waitKey(0)
+
+# Grayscale
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Find Canny edges
+edged = cv2.Canny(image, 30, 200)
+cv2.waitKey(0)
+
+# Finding Contours
+# Use a copy of the image e.g. edged.copy()
+# since findContours alters the image
+contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_NONE)
+
+cv2.imshow('Canny Edges After Contouring', edged)
+cv2.waitKey(0)
+
+print("Number of Contours found = " + str(len(contours)))
+
+# Draw all contours
+# -1 signifies drawing all contours
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+cv2.imshow('Contours', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # draw rect area around contours
-
-
