11ecde0b75f9b3cc9f08c415de0032156375c3ca
Soumik <sadmanks@gmail.com>
2020-02-22 21:02:55 +0600

    print duplicate elements from a list

---
+++ basic_programs/print_duplicate_elements_from_list.py
@@ -0,0 +1,12 @@
+def find_duplicates(ar):
+    duplicates = []
+    for i in range(0, len(ar)):
+        for j in range(i + 1, len(ar)):
+            if (ar[i] == ar[j]):
+                duplicates.append(ar[j])
+    return duplicates
+
+
+if __name__ == "__main__":
+    array = [1, 2, 4, 5, 2, 4, 10]
+    print(find_duplicates(array))
