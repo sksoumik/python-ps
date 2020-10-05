dfbd6fc5fd2ededcd4b50cc416447d36048b5da6
Sadman Kabir Soumik <sadmanks@gmail.com>
2020-10-05 21:03:51 +0600

    array left rotate

---
+++ hacker_rank_problem_solving/array_left_rotate.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/ctci-array-left-rotation 
+
+def rotLeft(a, d):
+    a = list(a)
+    return a[d:] + a[:d]
+
+
+if __name__ == "__main__":
+    n, d = map(int, input().strip().split(" "))
+    a = map(int, input().strip().split(" "))
+    left_rotated_array = rotLeft(a, d)
+    print(*left_rotated_array, sep=" ")
